f8f09877df5ec45c5effcf54b50d40d66421db09
yuyang <1184698361@qq.com>
2019-09-09 16:14:00 +0800

    观察者模式1

---
+++ app/src/main/java/com/alonsol/demo/observerdemo/demo1/Juejin.java
@@ -0,0 +1,13 @@
+package com.alonsol.demo.observerdemo.demo1;
+
+import java.util.Observable;
+
+public class Juejin extends Observable {
+
+    public void postNewPublication(String content) {
+        //标识状态或者内容发生改变
+        setChanged();
+        //通知所有观察者
+        notifyObservers(content);
+    }
+}
